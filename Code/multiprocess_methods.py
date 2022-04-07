978155cdcc6f773a2d9eb1fb7dcc93ed687afbb8
Feiyu Zheng <feiyuzheng98@gmail.com>
2022-04-07 03:26:45 -0400

    Add multiprocess sample code

--- Code/multiprocess_methods.py
+++ Code/multiprocess_methods.py
@@ -1,4 +1,4 @@
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool, cpu_count, Pipe, Process
 from tqdm import tqdm
 import numpy as np
 import json
@@ -47,27 +47,86 @@ def splitDataset(dataPath:str, totalSize:int, selectedKeys:list, outputPath:str,
     pool.close()
     pool.join()
 
+
+def consumer(pipe, outputPath, sampleSize):
+    _outPipe, _inPipe = pipe
+    _inPipe.close()
+
+    # with tqdm(total=sampleSize, desc="Processing", leave=True, unit_scale=True) as pbar:
+    with open(outputPath, 'w', newline='') as outputFile:
+        csvWriter = csv.writer(outputFile)
+        while True:
+            try:
+                line = _outPipe.recv()
+                csvWriter.writerow(line)
+                # pbar.update()
+            except EOFError:
+                break
+
+def sampleDataset(dataPath:str, totalSize:int, outputPath:str, sampleSize:int, printParameters:bool = False):
+    """
+    multiprocess sample function
+
+    Args:
+        dataPath (str): original dataset path
+        totalSize (int): total size of original dataset
+        outputPath (str): output dataset path
+        sampleSize (int): size of sample dataset
+        printParameters (bool, optional): show the parameters or not. Defaults to False.
+    """
+
+    if printParameters:
+        print(f"Dataset File: {dataPath}")
+        print(f"Total Size: {totalSize}")
+        print(f"Output File: {outputPath}")
+        print(f"Sample size: {sampleSize}")
+
+    outPipe, inPipe = Pipe()
+
+    consumerPc = Process(target = consumer, args = ((outPipe, inPipe), outputPath, sampleSize))
+    consumerPc.start()
+
+    sampleIndicesList = sorted(random.sample(range(totalSize), sampleSize))
+
+    outPipe.close()
+    with tqdm(total=sampleSize, desc="Processing", leave=True, unit_scale=True) as pbar:
+        with open(dataPath, encoding="utf-8", newline='') as file:
+            csvReader = csv.reader(file)
+            csvHeader = next(csvReader)
+            inPipe.send(csvHeader)
+
+            sampleIndex = 0
+            for i, line in enumerate(csvReader):
+                if i == sampleIndicesList[sampleIndex]:
+                    inPipe.send(line)
+                    sampleIndex += 1
+                    pbar.update()
+                if sampleIndex >= sampleSize:
+                    break
+    inPipe.close()
+    consumerPc.join()
+
 if __name__ == '__main__':
-    # input_csv_path = "Data/All_Amazon_Review_User_Item_Rating.csv"
-    # output_csv_path = "Data/test.csv"
-    # total_records = 157260921
-    # required_records = 2500000
-    # processNum = cpu_count()
-    # chunkSize = 1024
-    # try:
-    #     sampleDataset(input_csv_path, total_records, output_csv_path, required_records, processNum, chunkSize, True)
-    # except Exception as e:
-    #     print(e)
-    #     exit()
-    # print(selectedKeys)
-    selectedKeys = ['reviewerID', 'asin', 'overall']
-    dataPath = "Data/All_Amazon_Review_5.json"
-    totalSize = 157260921
-    outputPath = "Data/test.csv"
-    processNum = cpu_count() # number of processes (customize this variable based on different CPUs)
-    chunkSize = 1024 # the size of each chunk splitted from the iterable
+    input_csv_path = "Data/All_Amazon_Review_User_Item_Rating.csv"
+    output_csv_path = "Data/test.csv"
+    total_records = 157260921
+    required_records = 2500000
+    processNum = cpu_count()
+    chunkSize = 1024
     try:
-        splitDataset(dataPath, totalSize, selectedKeys, outputPath, processNum, chunkSize, True)
+        sampleDataset(input_csv_path, total_records, output_csv_path, required_records, True)
     except Exception as e:
         print(e)
         exit()
+
+    # selectedKeys = ['reviewerID', 'asin', 'overall']
+    # dataPath = "Data/All_Amazon_Review_5.json"
+    # totalSize = 157260921
+    # outputPath = "Data/test.csv"
+    # processNum = cpu_count() # number of processes (customize this variable based on different CPUs)
+    # chunkSize = 1024 # the size of each chunk splitted from the iterable
+    # try:
+    #     splitDataset(dataPath, totalSize, selectedKeys, outputPath, processNum, chunkSize, True)
+    # except Exception as e:
+    #     print(e)
+    #     exit()
